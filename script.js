5432baff39efe5957ad09d81935215c41652f45d
Wendall Wylie <wendallwylie81@gmail.com>
2024-07-05 09:34:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  
+  var spins = 0;
+	$('.spin-button').click(function() {
+    spins++;
+		var randDegree = Math.floor(Math.random() * 1800) + 1;
+    var totalDegree = spins * 1800 + randDegree;
+		$('#wheel').css({'transform' : 'rotate(' + totalDegree + 'deg)'});
+	});
+  
+  $('.fa-heart').click(function() {
+    $('.overlay').fadeIn();
+  })
+  
+  $('.overlay').click(function() {
+    $(this).fadeOut();
+  })
+
+});
+var audio = document.getElementById('audio');
+var isPlaying = false;
+
+$('.btn-play-music').click(function(){
+    $(this).toggleClass('pause');
+    $(this).parent('.music-box').toggleClass('playing');
+    $(this).find('.fa').toggleClass('fa-pause');
+    if (isPlaying) {
+      audio.pause()
+    } else {
+      audio.play();
+    }
+});
+audio.onplaying = function() {
+  isPlaying = true;
+};
+audio.onpause = function() {
+  isPlaying = false;
+};
